32a03797db19ae0e8e4f064a3cc3f2d468f25ac7
A.Switch <hello@awitch.me>
2023-07-18 21:09:08 +0200

    Initial Commit

---
+++ Bored/BoredApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoredApp.swift
+//  Bored
+//
+//  Created by user1 on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct BoredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
